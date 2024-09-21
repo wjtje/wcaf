08d5f755e776b1dead5619944a82e8bdb4de40d3
Wouter <33957974+wjtje@users.noreply.github.com>
2024-09-21 14:48:40 +0200

    Made the component class pure virtual

--- src/wcaf/core/component.h
+++ src/wcaf/core/component.h
@@ -4,9 +4,9 @@ namespace wcaf {
 
 class Component {
  public:
-  virtual void setup();
-  virtual void loop();
-  virtual const char* get_tag();
+  virtual void setup() = 0;
+  virtual void loop() = 0;
+  virtual const char* get_tag() = 0;
 };
 
 }  // namespace wcaf
